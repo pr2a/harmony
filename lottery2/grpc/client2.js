419c5cf4cf5c1797a08f0a7b4f02300dc7331531
Minh Doan <minh@harmony.one>
2019-03-19 23:58:17 -0700

    add grpc protobuf

--- lottery2/grpc/client2.js
+++ lottery2/grpc/client2.js
@@ -1,42 +1,66 @@
-const PROTO_PATH = __dirname + '/message.proto';
-
+const msg = require('./message_pb');
+const services = require('./message_grpc_pb');
 const grpc = require('grpc');
-const protoLoader = require('@grpc/proto-loader');
-const packageDefinition = protoLoader.loadSync(PROTO_PATH, {
-  keepCase: true,
-  longs: String,
-  enums: String,
-  defaults: true,
-  oneofs: true
-});
-const messageProto = grpc.loadPackageDefinition(packageDefinition).message;
 
-const process = msg =>
-  function main() {
-    let client = new messageProto.ClientService(
-      'localhost:30000',
-      grpc.credentials.createInsecure()
-    );
-    let user;
-    if (process.argv.length >= 3) {
-      user = process.argv[2];
-    } else {
-      user = 'world';
-    }
-    client.sayHello({ name: user }, function(err, response) {
-      console.log('Greeting:', response.message);
-    });
-  };
+const client = new services.ClientServiceClient(
+  'localhost:30000',
+  grpc.credentials.createInsecure()
+);
 
-main();
+const processEnter = (key, amount) => {
+  let message = createMessage('ENTER', key, amount);
 
-// // Process processes message.
-// func(client * Client) Process(message * Message) * Response {
-//     ctx, cancel := context.WithTimeout(context.Background(), 10 * time.Second)
-//     defer cancel()
-//     response, err := client.clientServiceClient.Process(ctx, message)
-//     if err != nil {
-//         log.Fatalf("Getting error when processing message: %s", err)
-//     }
-//     return response
-// }
+  client.process(message, function(err, response) {
+    console.log('Greeting:', response);
+  });
+};
+
+const processWinner = () => {
+  let message = createMessage('WINNER');
+
+  client.process(message, function(err, response) {
+    console.log('Greeting:', response);
+  });
+};
+
+const processResult = key => {
+  let message = createMessage('RESULT', key);
+
+  client.process(message, function(err, response) {
+    console.log('Greeting:', response);
+  });
+};
+
+const createMessage = (requestType, key = null, amount = null) => {
+  let message = new msg.Message();
+  let lotteryRequest = new msg.LotteryRequest();
+  switch (requestType) {
+    case 'ENTER':
+      lotteryRequest.setType(msg.LotteryRequest.Type.ENTER);
+      break;
+    case 'RESULT':
+      lotteryRequest.setType(msg.LotteryRequest.Type.RESULT);
+      break;
+    case 'WINNER':
+      lotteryRequest.setType(msg.LotteryRequest.Type.PICK_WINNER);
+      break;
+  }
+  if (key) {
+    lotteryRequest.setPrivateKey(key);
+  }
+  if (amount) {
+    lotteryRequest.setAmount(amount);
+  }
+
+  message.setReceiverType(msg.ReceiverType.CLIENT);
+  message.setServiceType(msg.ServiceType.CLIENT_SUPPORT);
+  message.setType(msg.MessageType.LOTTERY_REQUEST);
+  message.setLotteryRequest(lotteryRequest);
+  return message;
+};
+
+module.exports = {
+  processEnter,
+  processWinner,
+  processResult
+};
--- lottery2/server.js
+++ lottery2/server.js
@@ -1,22 +1,36 @@
+const {
+  processEnter,
+  processWinner,
+  processResult
+} = require('./grpc/client2');
 const express = require('express');
 const app = express();
-const PORT = 9000;
+
+const PORT = 60000;
 
 app.get('/enter/:key&:amount', (req, res) => {
   const key = req.params.key;
   const amount = req.params.amount;
-  console.log(key, ' ', amount);
-  res.json({});
+  console.log('received ENTER request with', key, ' ', amount);
+  const r = processEnter(key, amount);
+
+  console.log('result:', r);
+  res.json(r);
 });
 
 app.get('/winner', (req, res) => {
-  res.json({});
+  console.log('received WINNER request with');
+  const r = processWinner();
+  console.log('result:', r);
+  res.json(r);
 });
 
 app.get('/result/:key', (req, res) => {
   const key = req.params.key;
-  console.log(key);
-  res.json({});
+  console.log('received RESULT request with', key);
+  const r = processResult(key);
+  console.log('result:', r);
+  res.json(r);
 });
 
 app.listen(PORT, err => {
