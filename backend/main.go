b5349375ec02037592c2338a4c9f7557603b837c
Minh Doan <minh@harmony.one>
2019-04-19 12:14:23 -0700

    add examples of reading data

--- backend/main.go
+++ backend/main.go
@@ -6,22 +6,31 @@ import (
 	"log"
 
 	"cloud.google.com/go/firestore"
+	"google.golang.org/api/iterator"
+	"google.golang.org/api/option"
 )
 
-func AddData() {
-	_, _, err = client.Collection("users").Add(ctx, map[string]interface{}{
-		"first": "Ada",
-		"last":  "Lovelace",
-		"born":  1815,
-	})
-	if err != nil {
-		log.Fatalf("Failed adding alovelace: %v", err)
-	}
+const (
+	TestPlayers = "test_players"
+)
 
+// Find more examples here: https://cloud.google.com/firestore/docs/quickstart-servers
+
+// AddData --
+func AddData(client *firestore.Client) {
+	// _, _, err = client.Collection("users").Add(ctx, map[string]interface{}{
+	// 	"first": "Ada",
+	// 	"last":  "Lovelace",
+	// 	"born":  1815,
+	// })
+	// if err != nil {
+	// 	log.Fatalf("Failed adding alovelace: %v", err)
+	// }
 }
 
-func ReadData() {
-	iter := client.Collection("users").Documents(ctx)
+// ReadData --
+func ReadData(client *firestore.Client, ctx context.Context) {
+	iter := client.Collection(TestPlayers).Documents(ctx)
 	for {
 		doc, err := iter.Next()
 		if err == iterator.Done {
@@ -39,16 +48,14 @@ func main() {
 	projectID := "benchmark-209420"
 	// Get a Firestore client.
 	ctx := context.Background()
-	client, err := firestore.NewClient(ctx, projectID)
+	opt := option.WithCredentialsFile("./keys/benchmark_account_key.json")
+	client, err := firestore.NewClient(ctx, projectID, opt)
 	if err != nil {
 		log.Fatalf("Failed to create client: %v", err)
 	}
 
 	// Close client when done.
 	defer client.Close()
-	opt := option.WithCredentialsFile("./keys/.json")
-	app, err := firebase.NewApp(context.Background(), nil, opt)
-	if err != nil {
-		return nil, fmt.Errorf("error initializing app: %v", err)
-	}
+
+	ReadData(client, ctx)
 }
