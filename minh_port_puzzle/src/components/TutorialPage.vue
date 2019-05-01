11c3661fb894119fd339434dc00058fe2eaa568e
Richard Liu <liuzhenglaichn@gmail.com>
2019-05-01 14:44:35 -0700

    fix tutorial text

--- minh_port_puzzle/src/components/TutorialPage.vue
+++ minh_port_puzzle/src/components/TutorialPage.vue
@@ -12,10 +12,24 @@
   overflow: auto;
 }
 .msg {
-  padding: 0.5em;
+  padding: 1em;
+  border-radius: 0.5em;
   color: #59504d;
   font-family: Fira Sans, sans-serif;
-  font-size: 0.8em;
+  border: 0.15em solid #979797;
+  height: 10em;
+  overflow: auto;
+  background-color: #fff;
+  li {
+    list-style: none;
+    &:before {
+      content: "\1F449";
+      margin-right: 0.5em;
+    }
+  }
+  li + li {
+    margin-top: 0.5em;
+  }
 }
 
 .btn-primary {
@@ -28,9 +42,13 @@
   <div class="tutorial-page">
     <div class="content">
       <div class="logo"></div>
-      <div
-        class="msg"
-      >Here is the tutotial text. Tell players the rules Here is the tutotial text. Tell players the rules Here is the tutotial text. Tell players the rules Here is the tutotial text. Tell players the rules Here is the tutotial text. Tell players the rules Here is the tutotial text. Tell players the rules Here is the tutotial text. Tell players the rules Here is the tutotial text. Tell players the rules Here is the tutotial text. Tell players the rules</div>
+      <div class="msg">
+        <ul>
+          <li>Swipe or use arrow keys to move between chips</li>
+          <li>The number on chip increments by one when you move onto it</li>
+          <li>You complete the level once all the chips have the same number</li>
+        </ul>
+      </div>
     </div>
     <button class="btn-primary" @click="$emit('start')">Got it</button>
   </div>
@@ -38,10 +56,6 @@
 
 <script>
 export default {
-  name: "TutorialPage",
-
-  data() {
-    return {};
-  }
+  name: "TutorialPage"
 };
 </script>
