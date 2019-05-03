22fc601c19457d80e3ed85f2e02c858f9abd95d4
Minh Doan <minh@harmony.one>
2019-05-03 12:28:24 -0700

    add google analytics, build and add to frontend for deploy

--- minh_port_puzzle/src/main.js
+++ minh_port_puzzle/src/main.js
@@ -1,12 +1,26 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
-import router from './router'
+import Vue from 'vue';
+import VueAnalytics from 'vue-analytics';
+import App from './App';
 import './filter';
 import './icon';
+import router from './router';
 
-Vue.config.productionTip = false
+Vue.use(VueAnalytics, {
+  id: 'UA-122659990-3'
+});
+// <!-- Global site tag (gtag.js) - Google Analytics -->
+// <script async src="https://www.googletagmanager.com/gtag/js?id=UA-122659990-3"></script>
+// <script>
+//   window.dataLayer = window.dataLayer || [];
+//   function gtag(){dataLayer.push(arguments);}
+//   gtag('js', new Date());
+
+//   gtag('config', 'UA-122659990-3');
+// </script>
+
+Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
 new Vue({
@@ -14,4 +28,4 @@ new Vue({
   router,
   components: { App },
   template: '<App/>'
-})
+});
