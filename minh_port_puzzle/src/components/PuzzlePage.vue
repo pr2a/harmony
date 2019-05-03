8a42425649b8be8e76e13ca61c24542cfa9c78a3
ak <alok@harmony.one>
2019-05-03 11:14:15 -0700

    stop music

--- minh_port_puzzle/src/components/PuzzlePage.vue
+++ minh_port_puzzle/src/components/PuzzlePage.vue
@@ -371,7 +371,7 @@ export default {
       store.data.stake = 20;
       clearInterval(this.timer);
       this.timer = null;
-      playPostGameMusic();
+      //playPostGameMusic();
     },
     restart() {
       this.gameEnded = false;
