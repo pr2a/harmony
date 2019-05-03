81cdd3ef8bc44cddafd8e7d6e14bea15428c42b6
ak <alok@harmony.one>
2019-05-03 16:40:34 -0700

    sound js and remve post game music

--- minh_port_puzzle/src/components/PuzzlePage.vue
+++ minh_port_puzzle/src/components/PuzzlePage.vue
@@ -382,7 +382,6 @@ export default {
       store.data.stake = 20;
       clearInterval(this.timer);
       this.timer = null;
-      //playPostGameMusic();
     },
     restart() {
       this.gameEnded = false;
