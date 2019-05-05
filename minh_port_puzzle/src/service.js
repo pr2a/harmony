f8c7c30e8fb8dcbbde4208ced28a74ec57a42b11
Leo Chen <leo@harmony.one>
2019-05-05 16:31:04 -0700

    [puzzlebe] update the backend url
    It is reported the backend url appspot.com is not accessible in China,
    thus the game is not playable.
    
    We added a domain name bepuzzle.harmonyprotocol.com to use the BE
    service.
    
    Signed-off-by: Leo Chen <leo@harmony.one>

--- minh_port_puzzle/src/service.js
+++ minh_port_puzzle/src/service.js
@@ -2,8 +2,11 @@ import axios from "axios";
 import store from "./store";
 
 // const HTTP_BACKEND_URL = `https://us-central1-harmony-puzzle.cloudfunctions.net`;
-const HTTP_BACKEND_URL = `https://harmony-puzzle-backend.appspot.com`;
+// const HTTP_BACKEND_URL = `https://harmony-puzzle-backend.appspot.com`;
 // const HTTP_BACKEND_URL = `https://d17b3244-d36f-40a1-959d-6a289de67a5b.mock.pstmn.io/`;
+
+const HTTP_BACKEND_URL = `https://bepuzzle.harmonyprotocol.com`;
+
 function sendPost(url, params) {
     return axios.post(HTTP_BACKEND_URL + url, params, {
         headers: {
