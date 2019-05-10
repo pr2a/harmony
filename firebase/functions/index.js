d05fe6954f81e28ad7f77bfc23f2eaeb19424b94
Minh Doan <minh@harmony.one>
2019-05-09 17:40:28 -0700

    remove unused code

--- firebase/functions/index.js
+++
@@ -1,8 +0,0 @@
-const functions = require('firebase-functions');
-// The Firebase Admin SDK to access the Firebase Realtime Database.
-const admin = require('firebase-admin');
-admin.initializeApp();
-
-const app = require('./lottery2/server');
-
-exports.helloWorld = functions.https.onRequest(app);
--- firebase/functions/lottery2/grpc/gen.sh
+++
@@ -1,1 +0,0 @@
-grpc_tools_node_protoc --js_out=import_style=commonjs,binary:./ --grpc_out=./ --plugin=protoc-gen-grpc=`which grpc_tools_node_protoc_plugin` message.proto
