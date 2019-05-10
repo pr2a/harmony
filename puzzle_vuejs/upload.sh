9d03c41acad4adf167afae18d8a8d07e0e1a9efa
Leo Chen <leo@harmony.one>
2019-05-10 14:16:50 -0700

    improve upload.sh to auto add static files
    Signed-off-by: Leo Chen <leo@harmony.one>

--- puzzle_vuejs/upload.sh
+++ puzzle_vuejs/upload.sh
@@ -1,3 +1,12 @@
-rm -rf ../puzzle_frontend/*
+#!/bin/bash
+
+rm -rf ../puzzle_static/*
 npm run build
-cp -rf dist/* ../puzzle_frontend/
+cp -rf dist/* ../puzzle_static/
+
+git statu
+
+git add -u
+git add ../puzzle_static/static
+
+git statu
