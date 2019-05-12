5283b79707b772528170a9187b2ba924bfe65136
Leo Chen <leo@harmony.one>
2019-05-11 17:52:54 -0700

    update backend to use official testnet backend
    update frontend to use https://explorer.harmony.one as the explorer
    
    Signed-off-by: Leo Chen <leo@harmony.one>

--- puzzle_vuejs/src/components/PuzzlePage.vue
+++ puzzle_vuejs/src/components/PuzzlePage.vue
@@ -175,7 +175,7 @@ footer {
       <div class="game-container" ref="gameContainer">
         <!--<redeem-panel v-if="gameEnded && !globalData.email" :reward="reward"></redeem-panel>-->
         <a
-          :href="'https://explorer2.harmony.one/#/address/' + globalData.address"
+          :href="'https://explorer.harmony.one/#/address/' + globalData.address"
           class="logo"
           target="_blank"
         ></a>
@@ -252,7 +252,7 @@ footer {
         </footer>
         <div class="link-footer" v-if="!isMobile">
           <a
-            :href="'https://explorer2.harmony.one/#/address/' + globalData.address"
+            :href="'https://explorer.harmony.one/#/address/' + globalData.address"
             target="_blank"
             class="link"
           >View Transactions</a>
