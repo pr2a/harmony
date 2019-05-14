b39f6b09781d587b818a0cbdbbb73eccc22b2f1e
Eugene Kim <ek@harmony.one>
2019-05-14 16:33:11 -0700

    Create keys directory if it doesn't exist

--- lottery/server/key_copy.sh
+++ lottery/server/key_copy.sh
@@ -1,1 +1,2 @@
+mkdir -p functions/keys
 cp ../../../secret_keys/firebase/benchmark-firebase-db-key.json ./functions/keys/
--- puzzle/puzzle_backend/key_copy.sh
+++ puzzle/puzzle_backend/key_copy.sh
@@ -1,1 +1,2 @@
+mkdir -p keys
 cp ../../../secret_keys/firebase/benchmark-firebase-db-key.json keys/
